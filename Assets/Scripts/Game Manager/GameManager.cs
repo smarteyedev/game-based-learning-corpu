e5894e52de403339f514e486c574b75476ac115e
Muhammad Taufiq Shidiq <shantaufiq021@gmail.com>
2025-04-24 10:25:34 +0700

    chore: textmeshpro

--- Assets/Scripts/Game Manager/GameManager.cs
+++ Assets/Scripts/Game Manager/GameManager.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Scenes")]
+    [SerializeField] private SceneField sceneMenu;
+    [SerializeField] private SceneField sceneMainMaps;
 
-    // Update is called once per frame
-    void Update()
+    private void StartGame()
     {
-        
+        SceneManager.LoadSceneAsync(sceneMenu, LoadSceneMode.Additive);
     }
 }
